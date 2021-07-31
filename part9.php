7548cb5fc39e31a567a9e4bb46c3b23bc37a45b9
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-07-31 02:20:49 -0400

    added functions to part9.php

--- part9.php
+++ part9.php
@@ -10,4 +10,21 @@
     $dogYears = $age/7; //Arithmetic Operator
     $isAdult = $dogYears > 4;; //Comparison operator
     $isSettled = $isAdult and $married; //Interger type variable
+
+    function getFullName($firstName, $lastName) { //Function name
+        return $firstName . " " . $lastName; //Returned value
+    }
+
+    $fullname = getFullName($firstName, $lastName); //Using variables
+    $fullName = getFullName("Alice", "Doe"); //Using values
+
+    function getIsSettled($age, $married) { //Function Name
+        $dogYears = $age/7;
+        $isAdult = $dogYears > 4;
+        $isSettled = $isAdult and $married;
+        return $isSettled; //Returned value
+    }
+
+    $isSettled = getIsSettled($age, $married); //Using variables
+    $isSettled = getIsSettled(5, false); //Using values
 ?>
