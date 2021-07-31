c9e39936cb264d5d6534a77b0e8e317e2083d9f5
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-07-31 01:34:41 -0400

    added header.php and updated pages

---
+++ header.php
@@ -0,0 +1,9 @@
+<div id="header">
+        <header>
+            <h1>Blog CMS</h1>
+        </header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="post-submission.php">Post Submission</a>
+        </nav>
+    </div><!--close for header-->
--- post.php
+++ post.php
@@ -10,15 +10,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet"> 
 </head>
     <body>
-        <div id="header">
-            <header>
-                <h1>Blog CMS</h1>
-            </header>
-            <nav>
-                <a href="index.php">Home</a>
-                <a href="post-submission.php">Post Submission</a>
-            </nav>
-        </div><!--close for header-->
+    <?php include 'header.php' ?>
         <main>
             <h2>Blog Post 1</h2>
             <div id="datepublished">
