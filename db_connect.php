257f74135d0c3343414f26e59a4192426e06eb57
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-08-08 17:11:57 -0400

    created db connect

---
+++ db_connect.php
@@ -0,0 +1,8 @@
+<?php
+$dbServer = "localhost";
+$dbUsername = "root";
+$dbPassw0rd = "root";
+$dbName = "blog_cms";
+//Create Connection
+$conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
+?>
