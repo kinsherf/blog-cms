0b2dc293bb4b60ef1f2164d2148000b3f0911b85
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-08-14 15:54:36 -0400

    added variables to hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
 alert ("Hello World");
+
+var firstName = "Jane"; //String type variable
+var lastName = "Doe"; //String type variable
+var age = 35; //number type variable
+var married = true; //boolean type variable
+var childrenNames = ["Alice", "Bob"]; //Array type variable
+var husband = {firstName: "John", lastName: "Doe", age: 35} //object type variable
