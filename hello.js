64d6bd8f273f82bde864bb867bfcf8bb1cc4f0f8
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-08-14 15:56:44 -0400

    added operators to hello.js

--- hello.js
+++ hello.js
@@ -6,3 +6,8 @@ var age = 35; //number type variable
 var married = true; //boolean type variable
 var childrenNames = ["Alice", "Bob"]; //Array type variable
 var husband = {firstName: "John", lastName: "Doe", age: 35} //object type variable
+
+var fullName = firstName + " " + lastName; //concatentation operator
+var dogYears = age/7; //arithmetic operator
+var isAdult = dogYears > 4; //comparison operator
+var isSettled = isAdult && married; //integer type variable
